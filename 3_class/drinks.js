ef8b53e5b69bc709cd4fa92cbccae14367a09116
Andrei Zayats <andrei.zayats93@gmail.com>
2018-09-17 17:27:10 +0300

    1.1

--- 3_class/drinks.js
+++ 3_class/drinks.js
@@ -1,3 +1,4 @@
+"use strict";
 class HashStorage {
     constructor() {
         this.hash = {};
@@ -28,13 +29,49 @@ class HashStorage {
         console.log(this.hash);
     }
 }
-
+function htmlAddValue() {
+    let drink = prompt('Введите название напитка:')||" ",
+        alcohol = confirm('Этот напиток алкогольный?')||false,
+        recipe = prompt('Введите рецепт напитка:')||" ";
+    drinkStorage.addValue(drink, {'alcohol': alcohol, 'recipe': recipe});
+}
+function htmlGetValue() {
+    let drink = prompt('Введите название напитка:'),
+    drinkObj=drinkStorage.getValue(drink);
+    if (typeof drinkObj  !== 'undefined') {
+        console.log(`
+        напиток: ${drink}
+        алкогольный: ${drinkStorage.hash[drink]["alcohol"]}
+        рецепт приготовления: ${drinkStorage.hash[drink]["recipe"]}
+        `);
+    } else {
+        console.log(`Такого напитка нет`);
+    }
+}
+function htmlDeleteValue() {
+    let key = prompt('Введите название напитка:'),
+    status=drinkStorage.deleteValue(key);
+    if (status===true) {
+        alert(`Напиток ${key} удален`);
+    } else {
+        alert(`Напиток ${key} не найден`);
+    }
+}
+function htmlAllValue() {
+    let key = drinkStorage.getKeys(),
+    i;
+    for (i in key) {
+        console.log(`
+        напиток: ${key}
+        `);
+    }
+}
 const drinkStorage = new HashStorage();
-drinkStorage.addValue("Напиток 1", "Ингридиенты 1");
-drinkStorage.addValue("Напиток 2", "Ингридиенты 2");
-drinkStorage.getValue("Напиток 1");
-drinkStorage.getValue("Напиток 2");
+//drinkStorage.addValue("Напиток 1", "Ингридиенты 1");
+//drinkStorage.addValue("Напиток 2", "Ингридиенты 2");
+//drinkStorage.getValue("Напиток 1");
+//drinkStorage.getValue("Напиток 2");
 //drinkStorage.watchValue();
-console.log(drinkStorage.getKeys());
-drinkStorage.deleteValue("Напиток 1");
+//console.log(drinkStorage.getKeys());
+//drinkStorage.deleteValue("Напиток 1");
 //drinkStorage.watchValue();
