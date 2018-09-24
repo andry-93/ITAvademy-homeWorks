8d47c242e89b6e0aca8d59044bec8dcf435e5194
Andrei Zayats <andrei.zayats93@gmail.com>
2018-09-24 16:37:34 +0300

    1.8

--- 3_class/mood.js
+++ 3_class/mood.js
@@ -4,20 +4,33 @@ function randomDiap(n,m) {
     return Math.floor(Math.random()*(m-n+1))+n;
 }
 
-function mood(colorsCount) {
-    var colors=[ '', 'красный', 'оранжевый', 'жёлтый', 'зелёный', 'голубой', 'синий', 'фиолетовый' ],
-        resultColor={};
-
+/*function mood(colorsCount) {
+    const colors = ['', 'красный', 'оранжевый', 'жёлтый', 'зелёный', 'голубой', 'синий', 'фиолетовый'],
+        resultColor = {};
     console.log( 'цветов: ' + colorsCount );
-    for ( var i=1; i<=colorsCount; i++ ) {
-        var n=randomDiap(1,7);
+    for (let i=1; i<=colorsCount; i++ ) {
+        let n = randomDiap(1, 7);
         while (n in resultColor) {
             n=randomDiap(1,7);
         }
         resultColor[n]=colors[n];
-        var colorName=colors[n];
+        const colorName = colors[n];
+        console.log( colorName );
+    }
+}*/
+function mood(colorsCount) {
+    const colors = ['', 'красный', 'оранжевый', 'жёлтый', 'зелёный', 'голубой', 'синий', 'фиолетовый'],
+        resultColor = {};
+    console.log( 'цветов: ' + colorsCount );
+    for (let i=1; i<=colorsCount; i++ ) {
+        let n = randomDiap(1, 7);
+        while (colors[n] in resultColor) {
+            n=randomDiap(1,7);
+        }
+        resultColor[colors[n]]=n;
+        const colorName = colors[n];
         console.log( colorName );
     }
 }
 
-mood(3);
+mood(7);
