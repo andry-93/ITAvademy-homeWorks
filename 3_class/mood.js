b2877a9718b1ef5db8e9271073e448533414eb52
Andrei Zayats <andrei.zayats93@gmail.com>
2018-09-24 16:41:23 +0300

    1.9

--- 3_class/mood.js
+++ 3_class/mood.js
@@ -4,20 +4,6 @@ function randomDiap(n,m) {
     return Math.floor(Math.random()*(m-n+1))+n;
 }
 
-/*function mood(colorsCount) {
-    const colors = ['', 'красный', 'оранжевый', 'жёлтый', 'зелёный', 'голубой', 'синий', 'фиолетовый'],
-        resultColor = {};
-    console.log( 'цветов: ' + colorsCount );
-    for (let i=1; i<=colorsCount; i++ ) {
-        let n = randomDiap(1, 7);
-        while (n in resultColor) {
-            n=randomDiap(1,7);
-        }
-        resultColor[n]=colors[n];
-        const colorName = colors[n];
-        console.log( colorName );
-    }
-}*/
 function mood(colorsCount) {
     const colors = ['', 'красный', 'оранжевый', 'жёлтый', 'зелёный', 'голубой', 'синий', 'фиолетовый'],
         resultColor = {};
